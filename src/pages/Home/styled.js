00e9b421e437d5e24c5ffa0bae0a86d598f9133a
danilobrm <danilobr.web@gmail.com>
2022-12-21 22:32:17 -0300

    home-estilo

---
+++ src/pages/Home/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 90%;
+  height: fit-content;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
