24c9c2cf7e97e320e90aee86742b6c62a6f852e3
danilobrm <danilobr.web@gmail.com>
2022-12-21 22:32:49 -0300

    pagina-pokemon

---
+++ src/pages/Pokemon/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import About from '../../components/Pokemon/Sobre';
+import { Container } from './styled';
+
+export default function Pokemon() {
+  return (
+    <Container>
+      <About />
+    </Container>
+  );
+}
