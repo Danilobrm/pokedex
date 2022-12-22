5cb78ac75e8cb8584bf3630f1d3e76a70fe216f2
danilobrm <danilobr.web@gmail.com>
2022-12-21 22:32:05 -0300

    pagina-home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Pokedex from '../../components/Home/pokedex';
+import { Container } from './styled';
+
+export default function Home() {
+  return (
+    <Container>
+      <Pokedex />
+    </Container>
+  );
+}
