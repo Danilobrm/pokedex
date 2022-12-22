9db9f169e2da977b1ebda60e00bba7e9c8ce35b6
danilobrm <danilobr.web@gmail.com>
2022-12-21 22:22:43 -0300

    app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import store, { persistor } from './store';
+import Routes from './routes';
+import GlobalStyle from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Routes />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  );
+}
+
+export default App;
