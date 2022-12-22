ef7c814bb9a840f4fa4beaaf834d2ce3f2cff25d
danilobrm <danilobr.web@gmail.com>
2022-12-22 20:54:36 -0300

    estilo-pokedex

--- src/components/Home/pokedex/styled.js
+++ src/components/Home/pokedex/styled.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const PokedexStyle = styled.div`
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   margin: 0 auto;
@@ -9,35 +8,36 @@ export const PokedexStyle = styled.div`
   transition: 0.3s ease-in-out;
 
   .pokemon-card {
-    font-family: Poppins;
-    text-decoration: none;
-    margin: 10px;
-    width: 250px;
-    height: 250px;
     display: flex;
     flex-direction: column;
+    margin: 10px;
+    width: 250px;
+    height: fit-content;
     align-items: center;
-    text-align: center;
-    justify-content: flex-end;
     border-radius: 8px;
     border: 2px solid;
-    box-shadow: 0px 0px 10px black;
+    box-shadow: 0px 0px 3px black;
+    text-decoration: none;
 
     & .id {
       width: 80%;
+      padding: 10px 0;
       align-items: center;
       text-align: right;
     }
 
     & img {
-      width: 160px;
-      height: 160px;
+      width: 150px;
+      height: 150px;
     }
 
     & h1 {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 0;
+      align-items: center;
       width: 100%;
       height: 50px;
-      padding: 4px 8px 4px 8px;
       border-radius: 0 0 5px 5px;
       color: #fff;
       font-size: 25px;
@@ -48,23 +48,20 @@ export const PokedexStyle = styled.div`
 
     @media (max-width: 768px) {
       width: 130px;
-      height: 130px;
       & img {
         width: 60px;
         height: 60px;
       }
       & h1 {
         font-size: 15px;
+        height: 30px;
       }
       margin: 5px;
     }
   }
 
   @media (max-width: 768px) {
-    grid-template-columns: repeat(3, 1fr);
     transition: 0.3s ease-in-out;
-    justify-content: center;
     margin: 0 auto;
-    border: 1px solid red;
   }
 `;
