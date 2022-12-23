3452867a0eae961fbd8c7d211d59e616a2d3a1b7
danilobrm <danilobr.web@gmail.com>
2022-12-23 17:46:43 -0300

    estilo-pesquisa-pokemon

---
+++ src/components/Home/header/searchBar/styled.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const SearchBarStyle = styled.input`
+  font-family: Poppins;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 4px 8px;
+  gap: 16px;
+  width: 100%;
+  height: 40px;
+  border-radius: 8px;
+  background: #fff;
+  border: 1px solid #e0e0e0;
+  margin-bottom: 10px;
+
+  &::placeholder {
+    text-align: center;
+  }
+  &:focus {
+    border: 1px solid black;
+    &::placeholder {
+      color: transparent;
+    }
+  }
+
+  &:focus {
+    &:placeholder-shown {
+      text-align: left;
+      color: black;
+    }
+  }
+`;
