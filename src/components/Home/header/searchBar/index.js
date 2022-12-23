193cec0d024c22ac106561e7284231c06296238e
danilobrm <danilobr.web@gmail.com>
2022-12-23 17:46:30 -0300

    input-pesquisa-pokemon

---
+++ src/components/Home/header/searchBar/index.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import * as actions from '../../../../store/modules/pokemon-redux/actions';
+import { SearchBarStyle } from './styled';
+
+export default function SearchBar() {
+  const [value, setValue] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actions.pokemonSearchInputRequest({ value }));
+  });
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+  return (
+    <SearchBarStyle
+      type="search"
+      placeholder="Procurar"
+      value={value}
+      onChange={(e) => handleChange(e)}
+    />
+  );
+}
