3e422c3edbf945ff7c5d675370ff0d56c3f59f6f
danilobrm <danilobr.web@gmail.com>
2023-01-03 23:39:16 -0300

    remove-root-reducer

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@ import createSagaMiddleware from 'redux-saga';
 import persistedReducers from './modules/reduxPersist';
 
 import rootReducer from './modules/rootReducer';
-import rootSaga from './modules/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -13,8 +12,6 @@ const store = createStore(
   persistedReducers(rootReducer),
   applyMiddleware(sagaMiddleware)
 );
-
-sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 export default store;
