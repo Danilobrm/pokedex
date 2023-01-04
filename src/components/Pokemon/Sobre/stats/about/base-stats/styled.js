3866e444c16900f547ee73a4e61d62478cad21f6
danilobrm <danilobr.web@gmail.com>
2023-01-04 11:51:33 -0300

    style-base-stats

--- src/components/Pokemon/Sobre/stats/about/base-stats/styled.js
+++ src/components/Pokemon/Sobre/stats/about/base-stats/styled.js
@@ -8,78 +8,90 @@ export const BaseStatsStyle = styled.div`
   .stats {
     align-items: center;
     display: flex;
-    width: 90%;
+    width: 50%;
     margin: 0 auto;
-    &:nth-child(2) {
-      margin-top: 20px;
-    }
-  }
-
-  .stat-name {
-    width: 70px;
-    display: flex;
-    align-items: center;
-    justify-content: right;
-    margin-left: 20px;
-  }
-  .vertical-line {
-    border-right: 2px solid #e0e0e0;
-    padding: 0 10px;
-    height: 50px;
-  }
-
-  .base-stat {
-    width: 70px;
-    display: flex;
-    justify-content: right;
-    margin-top: 4px;
-    margin-right: 5%;
-  }
-
-  .line {
-    width: 100%;
-  }
-
-  .line-stat-back {
-    display: flex;
-    width: 100%;
-    float: left;
-    height: 10px;
-    border-radius: 8px;
-  }
-  .line-stat {
-    display: flex;
-    margin-bottom: -10px;
-    float: left;
-    height: 10px;
-    border-radius: 8px;
-  }
-
-  @media (max-width: 768px) {
-    width: 80%;
     font-size: 15px;
-
-    .stats {
-      &:nth-child(2) {
-        margin-top: 10px;
-      }
+    font-weight: 400;
+    line-height: 16px;
+    & div:first-child {
+      font-weight: 700;
+      line-height: 16px;
+      width: 70px;
+      display: flex;
+      align-items: center;
+      justify-content: right;
+      margin-left: 20px;
     }
 
-    .stat-name {
-      padding: 0;
-    }
-    .vertical-line {
+    & .vertical-line {
       border-right: 2px solid #e0e0e0;
       padding: 0 10px;
       height: 50px;
     }
 
-    .base-stat {
+    & div:nth-child(3) {
       width: 70px;
       display: flex;
       justify-content: right;
       margin-top: 4px;
       margin-right: 5%;
+    }
+
+    & div:last-child {
+      width: 100%;
+
+      & .line-stat-back {
+        display: flex;
+        width: 100%;
+        float: left;
+        height: 7px;
+        border-radius: 8px;
+        opacity: 0.2;
+      }
+      & .line-stat {
+        display: flex;
+        margin-bottom: -10px;
+        float: left;
+        height: 7px;
+        border-radius: 8px;
+        opacity: 1;
+      }
+    }
+
+    &:nth-child(2) {
+      margin-top: 20px;
+    }
+    @media (max-width: 768px) {
+      width: 90%;
+      font-size: 10px;
+
+      &:nth-child(2) {
+        margin-top: 10px;
+      }
+
+      .vertical-line {
+        border-right: 2px solid #e0e0e0;
+        padding: 0 10px;
+        height: 40px;
+      }
+
+      & div:nth-child(3) {
+        width: 70px;
+        display: flex;
+        justify-content: right;
+        margin-top: 4px;
+        margin-right: 5%;
+      }
+      & div:last-child {
+        width: 100%;
+
+        & .line-stat-back {
+          height: 4px;
+        }
+        & .line-stat {
+          height: 4px;
+        }
+      }
     }
   }
 `;
