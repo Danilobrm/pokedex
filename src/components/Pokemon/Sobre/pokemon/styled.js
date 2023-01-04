3b56b7aca892639677b644c060ab85155c8ee659
danilobrm <danilobr.web@gmail.com>
2023-01-03 23:54:38 -0300

    pokemon-screen-style-fix

--- src/components/Pokemon/Sobre/pokemon/styled.js
+++ src/components/Pokemon/Sobre/pokemon/styled.js
@@ -4,36 +4,37 @@ export const Container = styled.div`
   z-index: 0;
   width: 100%;
   height: 100%;
-  padding-bottom: 20px;
-
-  .backgroundPokeball {
-    opacity: 0.2;
-    position: fixed;
-    right: 0;
-    top: -50px;
-    z-index: 0;
-    width: 700px;
-  }
-
-  .pokemon {
-    display: flex;
-    margin: 0 auto;
-    width: 300px;
-    z-index: 1;
-    position: relative;
-    top: 30px;
-  }
-
-  @media (max-width: 768px) {
-    height: 100%;
-    .backgroundPokeball {
-      width: 400px;
-      top: 0;
-      right: -100px;
+  padding-bottom: 10px;
+  & div {
+    & img:nth-child(1) {
+      opacity: 0.2;
+      position: fixed;
+      right: 0;
+      top: -50px;
+      z-index: 0;
+      width: 700px;
     }
-
-    .pokemon {
-      width: 250px;
+    & .pokemon {
+      display: flex;
+      margin: 0 auto;
+      width: 500px;
+      z-index: 1;
+      position: relative;
+    }
+    @media (max-width: 768px) {
+      height: 100%;
+      & img:nth-child(1) {
+        width: 400px;
+        height: 400px;
+        right: -50px;
+        top: -50px;
+      }
+      & .pokemon {
+        width: 300px;
+        height: 300px;
+        margin-bottom: -90px;
+        top: 0;
+      }
     }
   }
 `;
